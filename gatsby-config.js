7f0e21c785320c1b937eefcc216caaa801ee5a58
Tyler O'Briant <tyler.obriant@wpengine.com>
2020-09-30 09:32:49 -0500

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-pages`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Link } from "gatsby"
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date)
+    .map(edge => (
+      <Link className="post-link" to={edge.node.frontmatter.slug}>
+        {edge.node.frontmatter.title}
+      </Link>
+    ))
+  return (
+    <div class="homepage">
+      <div class="header">coffee in; words out</div>
+      {Posts}
+    </div>
+  )
+}
+export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "YYYY-MM-DD")
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`
